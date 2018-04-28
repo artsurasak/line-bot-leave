ff22a6426527a252b7857c8e34c932b45f83928c
Surasak <artvkm@gmail.com>
2018-04-28 20:29:25 +0700

    commit

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+	let reply_token = req.body.events[0].replyToken
+	reply(reply_token)
+	res.sendStatus(200)
+})
 app.get("/", function(req, res) {
     res.send("home");
 });
 app.listen(port, () => {
  console.log(`listening on ${port}`);
 });
+
+function reply(reply_token){
+	let header = {
+		'Content-Type': 'application/json',
+        'Authorization': 'Bearer {wNGFPyRYMYL1ZuaxZBZN+gw/1FOMR52WrEGtybbWfLlXsyIJU+2NUrUB70DQzT1ID4Jhwh34P4mc3fFdIEI7rtjUToiUzOlxjmtEfS/mekbd01VYgAYZybnHi9y0Q3REK0oaVFJricEwTEehEYaOGgdB04t89/1O/w1cDnyilFU=}'
+	}
+	let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
 /*'use strict';
 
 const line = require('@line/bot-sdk');
