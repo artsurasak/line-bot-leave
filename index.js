870f3a48ac815893072c23c71dc3fe56f13f80c9
Surasak <artvkm@gmail.com>
2018-05-15 21:19:26 +0700

    tesst

--- index.js
+++ index.js
@@ -293,36 +293,8 @@ function reply(reply_token,event_text,userID,messageID) {
 			                              text: userDTL
 			                      }
 			                client.replyMessage(reply_token,msg)
-                        calculateNoLeave(strDate,endDate,strTime,endTime,function(noLeave){
-                          var days = noLeave.Days
-                          var hours = noLeave.Hours
-                           msg = {
-			                              type: 'text',
-			                              text: days
-			                      }
-			                client.replyMessage(reply_token,msg)
-                          data.AllowDateAppr(leaveType,userDTL[0].ROLE_ID,userDTL[0].EMP_CODE,days,function(result){
-                          	endTime = event_text
-                          			msg = {
-				                              type: 'text',
-				                              text: result
-				                            }
-                   //        	if (result == true){
-                   //        		 msg = {
-			                //               type: 'text',
-			                //               text: "กรุณาระบุสาเหตุการลา (ถ้ามี)\n ถ้าไม่มีเลือก Next"
-			                //             }
-			                // }else{
-			                // 	msg = {
-			                //               type: 'text',
-			                //               text: "จำนวนวัลาเกินที่กำนด"
-			                //             }
-			                // }
-                            client.replyMessage(reply_token,msg)
                           });
                        	})
-                    })
-                  })
                   .catch((err) => {
                           console.log('error')
                           console.log(err);
